76c9e3942efdc86a37560e0bd06240527b25486f
Gabriel <uviedo_gabriel@hotmail.com>
2020-10-20 19:41:56 -0300

    Making async methods

---
+++ DatingApp_API/DatingApp_API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using DatingApp_API.Data;
+using DatingApp_API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserByID(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
