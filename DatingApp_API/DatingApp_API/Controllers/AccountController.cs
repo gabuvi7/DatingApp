ec9e3e8904c5f26b4b08a9ee996aaeed4cc0c75f
Gabriel <uviedo_gabriel@hotmail.com>
2020-10-26 12:28:03 -0300

    WIP - Adding login method and DTO

--- DatingApp_API/DatingApp_API/Controllers/AccountController.cs
+++ DatingApp_API/DatingApp_API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using DatingApp_API.Data;
 using DatingApp_API.DTOs;
 using DatingApp_API.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -33,6 +34,23 @@ namespace DatingApp_API.Controllers
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+            
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if (!computedHash[i].Equals(user.PasswordHash[i])) return Unauthorized("Invalid password");
+            }
 
             return user;
         }
--- DatingApp_API/DatingApp_API/DTOs/LoginDTO.cs
+++ DatingApp_API/DatingApp_API/DTOs/LoginDTO.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatingApp_API.DTOs
 {
-    public class RegisterDTO
+    public class LoginDTO
     {
-        [Required]
         public string Username { get; set; }
-        [Required]
         public string Password { get; set; }
     }
 }
