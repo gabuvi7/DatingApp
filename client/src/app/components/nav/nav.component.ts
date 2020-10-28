ad633f55346ea200697e00b52116640312ad1472
Gabriel <uviedo_gabriel@hotmail.com>
2020-10-27 22:18:56 -0300

    WIP - Adding toggle button to navbar

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavComponent implements OnInit {
+  navbarOpen = false;
+  public clicked = false;
+  _element: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
+  public onClick(event): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.clicked = true;
+  }
+
+  @HostListener(`document:click`, [`event`])
+  private clickedOutside(event): void {
+    if (this.clicked) {
+      this._element.nativeElement
+        .querySelector(`.dropdown-menu`)
+        .classList.toggle(`show`);
+    }
+  }
 }
