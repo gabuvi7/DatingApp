e20a96eb0e1d399c17855f91f4f5f47ec17fdf2b
Gabriel <uviedo_gabriel@hotmail.com>
2020-10-29 22:17:53 -0300

    WIP - Adding service and login persistance

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { error } from 'protractor';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,16 +10,43 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   navbarOpen = false;
   model: any = {};
+  loggedIn = false;
 
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(
+      (user) => {
+        this.loggedIn = !!user;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
   login() {
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
     console.log(this.model);
   }
 }
